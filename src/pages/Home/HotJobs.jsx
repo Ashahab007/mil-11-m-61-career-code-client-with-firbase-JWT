8824c97bc49960cd65cab0cd85d6137e125bd613
Ashahab007 <ashahab007@gmail.com>
2025-06-01 19:50:42 +0600

    check

--- src/pages/Home/HotJobs.jsx
+++ src/pages/Home/HotJobs.jsx
@@ -12,10 +12,10 @@ const HotJobs = ({ jobsPromise }) => {
       <h1 className="text-5xl font-extrabold text-center mb-10">
         Hot Jobs of the Day
       </h1>
-      
+
       <p className="text-center mb-8">Total Jobs:{jobs.length}</p>
       <div className="min-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-        {jobs.map((job) => (
+        {jobs?.map((job) => (
           <JobsCard key={job._id} job={job}></JobsCard>
         ))}
       </div>
